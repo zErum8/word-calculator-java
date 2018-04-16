5141a16121f9cdac02051a0c3e988a03caadbadc
zErum8 <zeniateo@gmail.com>
2018-04-16 15:17:35 +0100

    Add generated maven packages

---
+++ src/main/java/lt/zerum8/wcj/App.java
@@ -0,0 +1,13 @@
+package lt.zerum8.wcj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
